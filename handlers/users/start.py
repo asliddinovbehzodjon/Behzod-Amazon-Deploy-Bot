6f8630057f126d8f8f052d4b81fae9608e108cbe
Behzod Asliddinov <behzodme@gmail.com>
2022-08-04 19:16:00 +0500

    behzod

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -2,8 +2,14 @@ from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
 
 from loader import dp
-
+from aiogram.types import ReplyKeyboardMarkup,KeyboardButton
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
-    await message.answer(f"Salom, {message.from_user.full_name}!")
+    btn = ReplyKeyboardMarkup(resize_keyboard=True)
+    a = KeyboardButton("Telefon raqamizni ulashing",request_contact=True)
+    btn.add(a)
+    await message.answer(f"Assalomu alaykum, {message.from_user.full_name}!",reply_markup=btn)
+@dp.message_handler(types.ContentTypes.CONTACT)
+async def contact(message:types.Message):
+    await message.answer("Kontakt qabul qilindi!")
